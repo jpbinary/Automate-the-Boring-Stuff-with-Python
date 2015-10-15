408a983ed157e42ec9d9f34faa4da6bf2cbed6c7
jpbinary <jp@iskyy.com>
2015-10-15 18:42:59 -0500

    section2 created

---
+++ Section2/if_else_example.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+password = 'swordfish'
+if password == 'swordfish':
+    print('Access granted.')
+else:
+    print('Access denied.')
---
+++ Section2/if_example.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+name = 'Bob'
+if name == 'Alice':
+    print('Hi Bob')
+print('Done')
